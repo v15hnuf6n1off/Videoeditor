9e09cf679c09cdf611f719898a617d66c6395e07
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2023-09-21 22:58:55 +0530

    Update trimmer.py

--- main/plugins/trimmer.py
+++ main/plugins/trimmer.py
@@ -28,7 +28,7 @@ from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
 
 async def trim(event, msg, st, et):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "𝚃𝚛𝚢𝚒𝚗𝚐 𝚃𝚘 𝙿𝚛𝚘𝚌𝚎𝚜𝚜", reply_to=msg.id)
     new_name = "out_" + dt.now().isoformat("_", "seconds")
     if hasattr(msg.media, "document"):
         file = msg.media.document
@@ -53,35 +53,35 @@ async def trim(event, msg, st, et):
         out = new_name + ext
     DT = time.time()
     try:
-        await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
+        await fast_download(name, file, Drone, edit, DT, "**𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍𝚒𝚗𝚐**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
+        return await edit.edit(f"𝚂𝚘𝚛𝚛𝚢 𝙰𝚗 𝚎𝚛𝚛𝚘𝚛 𝚘𝚌𝚌𝚞𝚛𝚎𝚍 𝚠𝚑𝚒𝚕𝚎 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍𝚒𝚗𝚐.\n\n𝙲𝚘𝚗𝚝𝚊𝚌𝚝 [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
     try:
-        await edit.edit("Trimming.")
+        await edit.edit("𝚃𝚛𝚒𝚖𝚖𝚒𝚗𝚐")
         bash(f'ffmpeg -i {name} -ss {st} -to {et} -acodec copy -vcodec copy {out}')
         out2 = new_name + '_2_' + '.mp4'
         rename(out, out2)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while trimming!\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        return await edit.edit(f"𝚂𝚘𝚛𝚛𝚢 𝙰𝚗 𝚎𝚛𝚛𝚘𝚛 𝚘𝚌𝚌𝚞𝚛𝚎𝚍 𝚠𝚑𝚒𝚕𝚎 𝚃𝚛𝚒𝚖𝚖𝚒𝚗𝚐..\n\n𝙲𝚘𝚗𝚝𝚊𝚌𝚝 [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     UT = time.time()
-    text = f"**TRIMMED by :** @{BOT_UN}"
+    text = f"**𝚃𝚛𝚒𝚖𝚖𝚎𝚍 𝙱𝚢** @{BOT_UN}"
     try:
         metadata = video_metadata(out2)
         width = metadata["width"]
         height = metadata["height"]
         duration = metadata["duration"]
         attributes = [DocumentAttributeVideo(duration=duration, w=width, h=height, supports_streaming=True)]
-        uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+        uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**𝚄𝚙𝚕𝚘𝚊𝚍𝚒𝚗𝚐 **')
         await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG3, attributes=attributes, force_document=False)
     except Exception:
         try:
-            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**UPLOADING:**')
+            uploader = await fast_upload(f'{out2}', f'{out2}', UT, Drone, edit, '**𝚄𝚙𝚕𝚘𝚊𝚍𝚒𝚗𝚐**')
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             print(e)
-            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+            return await edit.edit(f"𝚂𝚘𝚛𝚛𝚢 𝙰𝚗 𝚎𝚛𝚛𝚘𝚛 𝚘𝚌𝚌𝚞𝚛𝚎𝚍 𝚠𝚑𝚒𝚕𝚎 𝚄𝚙𝚕𝚙𝚊𝚍𝚒𝚗𝚐..\n\n𝙲𝚘𝚗𝚝𝚊𝚌𝚝 [SUPPORT]({SUPPORT_LINK})", link_preview=False)
     await edit.delete()
     os.remove(name)
     os.remove(out2)
