228b94f251554f21c017b60681131f496c587d71
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2023-09-24 21:30:51 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -26,7 +26,7 @@ from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_te
 async def start(event):
     await event.reply(f'{st}', 
                       buttons=[
-                              [Button.inline("Menu.", data="menu")]
+                              [Button.inline("Menu", data="menu")]
                               ])
     tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
     await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
@@ -37,7 +37,7 @@ async def menu(event):
     
 @Drone.on(events.callbackquery.CallbackQuery(data="info"))
 async def info(event):
-    await event.edit(f'**ℹ️NFO:**\n\n{info_text}',
+    await event.edit(f'**info:**\n\n{info_text}',
                     buttons=[[
                          Button.inline("Menu.", data="menu")]])
     
@@ -49,27 +49,27 @@ async def notice(event):
 async def source(event):
     await event.edit(source_text,
                     buttons=[[
-                         Button.url("FOR PERSONAL USE", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
-                         Button.url("FOR YOUR CHANNEL ", url="https://github.com/vasusen-code/videoconvertor/")]])
+                         
+                        Button.url("FOR YOUR CHANNEL ", url="https://github.com/vasusen-code/videoconvertor/")]])
                          
                     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
 async def help(event):
-    await event.edit('**👥HELP & SETTINGS**',
+    await event.edit('**settings**',
                     buttons=[[
-                         Button.inline("SET THUMB", data="sett"),
-                         Button.inline("REM THUMB", data='remt')],
+                         Button.inline("Set Thumb", data="sett"),
+                         Button.inline("Rem thumb", data='remt')],
                          [
-                         Button.inline("PLUGINS", data="plugins"),
-                         Button.inline("RESTART", data="restart")],
-                         [Button.url("SUPPORT", url=f"{SUPPORT_LINK}")],
+                         Button.inline("Features", data="plugins"),
+                         Button.inline("Restart", data="restart")],
+                         [Button.url("Support", url=f"{SUPPORT_LINK}")],
                          [
-                         Button.inline("BACK", data="menu")]])
+                         Button.inline("Back", data="menu")]])
     
 @Drone.on(events.callbackquery.CallbackQuery(data="plugins"))
 async def plugins(event):
     await event.edit(f'{help_text}',
-                    buttons=[[Button.inline("Menu.", data="menu")]])
+                    buttons=[[Button.inline("Menu", data="menu")]])
                    
  #-----------------------------------------------------------------------------------------------                            
     
